1637f1575a396d17e0d31cdd93b42557be8314d4
shixuewen <friendlysxw@163.com>
2021-11-22 15:25:58 +0800

    配置一个vue2的基础组件模板

---
+++ vue2-component.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="vue2__component"></div>
+</template>
+<script>
+export default {
+    name: 'Vue2Component',
+    components: {},
+    mixins: {},
+    props: {},
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+
+    },
+    watch: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    beforeDestroy() {
+
+    },
+    methods: {
+        
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .vue2__component{
+
+    }
+</style>
